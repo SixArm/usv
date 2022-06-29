7658e418e9e043209667ba336271a6d9a5ad564a
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-06-29 16:38:52 -0700

    Add comment about escape

--- bin/usv-print.bash
+++ bin/usv-print.bash
@@ -3,6 +3,7 @@ set -euf -o pipefail
 
 # USV example shell script that demonstrates the use of USV characters.
 # This script reads STDIN one character at a time, and prints output.
+# This deliberately does not do escape, to make this easier to read.
 
 while IFS= read -n1 -r c; do
     case  "$c" in
