683b5a7ec17e362d0bf3d13ce18f02a3008bf2fb
Joel Parker Henderson <joel@joelparkerhenderson.com>
2024-02-07 15:01:19 -0800

    Add stream

--- tests/stream/test.sh
+++ tests/stream/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -euf
 top="$(git rev-parse --show-toplevel)"
-cat input.usv | "$top/bin/usv-to-debug.bash" > output-actual.txt    
+cat "$top/examples/stream.usv" | "$top/bin/usv-to-debug.bash" > output-actual.txt    
 diff output-actual.txt output-expect.txt 
